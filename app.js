95e73063488513f056c9be6249aedcad44ba769b
Dan Black <danielb0318@gmail.com>
2020-09-02 18:48:48 -0400

    Complete phase-5 and day 1

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const { environment } = require('./config');
 const app = express();
 const indexRouter = require('./routes/index');
 const tweetsRouter = require('./routes/tweets')
+const cors = require('cors')
+app.use(cors({ origin: "http://localhost:4000" }));
 app.use(morgan("dev"));
 app.use(express.json())
 app.use('/index/', indexRouter)
 app.use('/tweets/', tweetsRouter)
+
 app.get("/", (req, res) => {
   res.send("Welcome to the express-sequelize-starter!");
 });
